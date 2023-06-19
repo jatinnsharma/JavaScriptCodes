0ba151154c9255d62418942605a8192cc76c715e
jatin <88488554+jatinnsharma@users.noreply.github.com>
2023-06-19 15:58:35 +0530

    Update L3_format currency.js

--- L3_format currency.js
+++ L3_format currency.js
@@ -16,6 +16,7 @@ myNum = myNum.toLocaleString("en-US", {style: "currency", currency: "USD"});
 //myNum = myNum.toLocaleString("hi-IN", {style: "currency", currency: "INR"});
 //myNum = myNum.toLocaleString("de-DE", {style: "currency", currency: "EUR"});
 
+
 //myNum = myNum.toLocaleString(undefined, {style: "percent"});
 //myNum = myNum.toLocaleString(undefined, {style: "unit", unit: "celsius"});
 
